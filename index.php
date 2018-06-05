d67ed42bbe490c6629b12ea0db07c78796d4bfe0
apk88 <31360070+apk88@users.noreply.github.com>
2018-06-05 11:41:04 -0700

    Intermediary

--- index.php
+++ index.php
@@ -3,15 +3,17 @@
 $filesToInclude = [
     '1-headprecustomcss',
     '2-customcss',
+    'add-to-cart-btn-styles',
     '3-fromcssthroughsvgs',
     '4-headerthroughsidebar',
     '5-ampsidebar',
-    '6 - maincontent',
+    'add-to-cart-btn',
+    //'6 - maincontent',
     '7 - footer'
 ];
 $html = '';
 foreach($filesToInclude as $fileToInclude) {
-    
+
     $html .= file_get_contents($fileToInclude . '.html');
 }
 
